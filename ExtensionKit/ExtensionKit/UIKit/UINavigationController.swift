382b6d68c85d1278c7c88fa37f9638fa425aa293
Carlos Grossi <carlos.grossi@gmail.com>
2018-02-28 15:20:39 -0300

    add UINavigationController extensions

---
+++ ExtensionKit/ExtensionKit/UIKit/UINavigationController.swift
@@ -0,0 +1,44 @@
+//
+//  UINavigationController.swift
+//  Pods
+//
+//  Created by Carlos Grossi on 19/06/17.
+//
+//
+
+import Foundation
+
+public extension UINavigationController {
+	
+	public var navigationBarSize: CGSize {
+		return navigationBar.frame.size
+	}
+	
+    override open var shouldAutorotate: Bool {
+		return visibleViewController?.shouldAutorotate ?? true
+	}
+	
+	override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		return visibleViewController?.supportedInterfaceOrientations ?? .all
+	}
+	
+	public func pushViewController(_ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void) {
+		CATransaction.begin()
+		CATransaction.setCompletionBlock(completion)
+		pushViewController(viewController, animated: animated)
+		CATransaction.commit()
+	}
+	
+	public var tabBarHeight: CGFloat? {
+        return tabBarController?.tabBar.bounds.height
+	}
+	
+	public func fadeTitle(to title: String, withDuration: TimeInterval = 0.1) {
+		let fadeTransition = CATransition()
+		fadeTransition.type = kCATransitionFade
+		fadeTransition.duration = withDuration
+		self.navigationBar.layer.add(fadeTransition, forKey: "fadeText")
+		self.viewControllers.first?.navigationItem.title = title
+	}
+	
+}
