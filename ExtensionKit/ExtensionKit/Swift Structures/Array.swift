0102aef4fd1ffe923d1e46c3627089c2e9b8d6d3
Carlos Grossi <carlos.grossi@gmail.com>
2018-02-28 14:46:56 -0300

    add Array extensions

---
+++ ExtensionKit/ExtensionKit/Swift Structures/Array.swift
@@ -0,0 +1,27 @@
+//
+//  Array.swift
+//  Pods
+//
+//  Created by Carlos Grossi on 19/06/17.
+//
+//
+
+public extension Array where Element:Equatable {
+	
+	public mutating func removeDuplicates() -> [Element] {
+		var result = [Element]()
+		for value in self {
+			if result.contains(value) == false {
+				result.append(value)
+			}
+		}
+		self = result
+		return self
+	}
+	
+	public func randomElement<T>() -> T? {
+		let index = arc4random_uniform(UInt32(self.count))
+		return self[Int(index)] as? T
+	}
+	
+}
