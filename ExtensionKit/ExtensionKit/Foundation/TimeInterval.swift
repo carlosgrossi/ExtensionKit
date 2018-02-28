03b0ffc0e9caff69daf84add7a9bd0b35cec16c5
Carlos Grossi <carlos.grossi@gmail.com>
2018-02-28 13:58:33 -0300

    add TimeInterval extensions

---
+++ ExtensionKit/ExtensionKit/Foundation/TimeInterval.swift
@@ -0,0 +1,21 @@
+//
+//  TimeInterval.swift
+//  Pods
+//
+//  Created by Carlos Grossi on 19/06/17.
+//
+//
+
+import Foundation
+
+public extension TimeInterval {
+	
+	/// Time interval duration in hours, minutes, seconds format
+	public var duration: String {
+        let seconds = Int(self) % 60
+        let minutes = (Int(self) / 60) % 60
+        let hours = (Int(self) / 3600)
+        return String(format: "%02d%@ %02d%@ %02d%@", hours, "h", minutes, "m", seconds, "s")
+	}
+	
+}
